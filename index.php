482570179c25fe1daf76cebf6085620deac0990f
tmurvv <tmurv@shaw.ca>
2019-03-21 13:39:08 -0600

    list responsive

--- index.php
+++ index.php
@@ -296,8 +296,7 @@
         <div class="login signatureBox" style="display: <?php echo $loginNeeded ? 'block' : 'none' ?>">
             <div class="login__line1">
                 <h3>Login<span> Page</span> </h3>
-                <h4 class="login__line1--signup"><a href="signup.php">Easy Sign-up</a></h4>
-                
+                <h4 class="login__line1--signup"><a href="signup.php">Easy Sign-up</a></h4>              
             </div>
             <p style="color: tomato;"><?php echo isset($result) ? $result : ''; ?></p>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="login__form">
@@ -450,15 +449,21 @@
                         <form method='post'>               
                             <div class="list__container--items-item">
                                 <div class="flex">
-                                    <div class="list__container--items-itemQty"><input type="text" value="<?php echo $item['qty']; ?>" disabled></div>
-                                    <div class="list__container--items-itemQtyPreEdit" hidden><input type="text" name="editQty" value="<?php echo $item['qty']; ?>"></div>
-                                    <div class="list__container--items-itemTitle" id='js--tempId'><input type="text" value="<?php echo $item['title']; ?>" disabled></div>            
-                                    <div class="list__container--items-itemTitlePreEdit" hidden><input type="text" name="editTitle" value="<?php echo $item['title']; ?>"></div>            
-                                    <div class="list__container--items-itemCheckBox"><input type="checkbox" name="checkBox" data-itemid="<?php echo $item['listItemId']; ?>" data-name="name" onclick="updateNumClicks(this.dataset.itemid, this.checked);" <?php echo $checked ?>></div>                                  
-                                    <button type='submit' class="list__container--items-itemEdit js--editItem"  name="editItem"><img src="./img/editItemIcon.png" alt="Pencil icon for edit list item"></button>                                  
-                                    <button type='submit' class="list__container--items-itemDelete" name='itemDelete'><img src="./img/deleteRedX.png" name="deleteItem" alt="Big red X icon for delete list item"></button>  
-                                    <div class="list__container--items-frecency" hidden><input type="text" name='frecency' value="<?php echo $item['calcfrec']; ?>"></div>                                                   
-                                    <div class="list__container--items-itemId" hidden><input type="text" name='editId' value="<?php echo $item['listItemId']; ?>"></div>                                                   
+                                    <div class="flex__line1">
+                                        <div class="list__container--items-itemQty"><input type="text" value="<?php echo $item['qty']; ?>" disabled></div>
+                                        <div class="list__container--items-itemQtyPreEdit" hidden><input type="text" name="editQty" value="<?php echo $item['qty']; ?>"></div>
+                                        <div class="list__container--items-itemTitle" id='js--tempId'><input type="text" value="<?php echo $item['title']; ?>" disabled></div>            
+                                        <div class="list__container--items-itemTitlePreEdit" hidden><input type="text" name="editTitle" value="<?php echo $item['title']; ?>"></div>            
+                                    </div>
+                                    <div class="flex__line2">
+                                        <!-- <div class="list__container--items-itemCheckBox"> -->
+                                            <!-- </div> -->
+                                        <input type="checkbox" name="checkBox" data-itemid="<?php echo $item['listItemId']; ?>" data-name="name" onclick="updateNumClicks(this.dataset.itemid, this.checked);" <?php echo $checked ?>>                                  
+                                        <button type='submit' class="list__container--items-itemEdit js--editItem"  name="editItem"><img src="./img/editItemIcon.png" alt="Pencil icon for edit list item"></button>                                                                         
+                                        <button type='submit' class="list__container--items-itemDelete" name='itemDelete'><img src="./img/deleteRedX.png" name="deleteItem" alt="Big red X icon for delete list item"></button>  
+                                        <div class="list__container--items-frecency" hidden><input type="text" name='frecency' value="<?php echo $item['calcfrec']; ?>"></div>                                                   
+                                        <div class="list__container--items-itemId" hidden><input type="text" name='editId' value="<?php echo $item['listItemId']; ?>"></div>
+                                    </div>                                                   
                                 </div>    
                             </div>
                         </form>    
